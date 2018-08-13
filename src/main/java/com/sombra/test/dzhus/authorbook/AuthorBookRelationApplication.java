9b42501f7b6e737b2e3ac6614960eb880743b1d5
Oleksii Dzhus <oleksa777@live.com>
2018-08-13 19:22:27 +0300

    Init commit.

---
+++ src/main/java/com/sombra/test/dzhus/authorbook/AuthorBookRelationApplication.java
@@ -0,0 +1,12 @@
+package com.sombra.test.dzhus.authorbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBookRelationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthorBookRelationApplication.class, args);
+  }
+}
