c8f5cec592c977809aa715acd14a1384156b41c7
Oleksii Dzhus <oleksa777@live.com>
2018-08-14 16:13:38 +0300

    Controller to handle error created.

--- src/main/java/com/sombra/test/dzhus/authorbook/controller/AuthorController.java
+++ src/main/java/com/sombra/test/dzhus/authorbook/controller/AuthorController.java
@@ -85,21 +85,6 @@ public class AuthorController {
     return "redirect:/";
   }
 
-  @ResponseStatus(HttpStatus.NOT_FOUND)
-  @ExceptionHandler(NotFoundException.class)
-  public ModelAndView handleNotFound(Exception exception) {
-
-    log.error("Handling not found exception");
-    log.error(exception.getMessage());
-
-    ModelAndView modelAndView = new ModelAndView();
-
-    modelAndView.setViewName("404error");
-    modelAndView.addObject("exception", exception);
-
-    return modelAndView;
-  }
-
   @InitBinder
   public void initBinder(WebDataBinder binder) {
     binder.registerCustomEditor(Date.class,
